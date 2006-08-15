907173f4371163d09a6a35c3872850b4c567f86c
adenilson <adenilson@56e0a439-041b-0410-8790-949b9ad245cc>
2006-08-15 03:14:47 +0000

    Only prints countour within a give threshold diameter.
    git-svn-id: file:///home/adenilson/temp/wtf/repository@16 56e0a439-041b-0410-8790-949b9ad245cc

--- thesis/output.cpp
+++ thesis/output.cpp
@@ -74,33 +74,35 @@ void print_contour(char *img_file_name, CvSeq *contours, float diam_thres) {
   float *diameters = NULL;
   int d_size = 0;
   
-  diameters = calc_diam(contours, &d_size);
-  
-  
+  diameters = calc_diam(contours, &d_size);  
 
-  for (; contourPtr != NULL; contourPtr = contourPtr->h_next) {
-    cvStartReadSeq(contourPtr, &reader);
-    n_point = contourPtr->total;
+  for (int i = 0; i < d_size; ++i)
+    if(diameters[i] > diam_thres) {
+  
+      cvStartReadSeq(contourPtr, &reader);
+      n_point = contourPtr->total;
 
-    cout << n_point << endl;
-    ++c_contour;
+      cout << n_point << endl;
+      ++c_contour;
     
-    stringstream i2s;
-    i2s << "_contour_" << c_contour << ".txt";
-    filename = img_file_name; filename += i2s.str();    
-    cout << filename << endl;
-    f_contour.open(filename.c_str());
+      stringstream i2s;
+      i2s << "_contour_" << c_contour << ".txt";
+      filename = img_file_name; filename += i2s.str();    
+      cout << filename << endl;
+      f_contour.open(filename.c_str());
+ 
+      for (int i = 0; i < n_point; ++i) {
+        CV_READ_SEQ_ELEM(p, reader);  	
+        //CV_REV_READ_SEQ_ELEM(p, reader);
+        f_contour << p.x << " " << p.y << endl;  		
+      }
 
-    for (int i = 0; i < n_point; ++i) {
-      CV_READ_SEQ_ELEM(p, reader);  	
-      //CV_REV_READ_SEQ_ELEM(p, reader);
-      f_contour << p.x << " " << p.y << endl;  		
+      f_contour.close();
+      contourPtr = contourPtr->h_next;
+      
     }
-
-    f_contour.close();
-
-  }
-
+    
+  
   sci_prog(c_contour, img_file_name);
 }
 //------------------------------------------------------------------------------
