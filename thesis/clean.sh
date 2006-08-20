2e62f3c42b2e1d272690fff1bc64d84bff899696
adenilson <adenilson@56e0a439-041b-0410-8790-949b9ad245cc>
2006-08-20 23:41:42 +0000

    Added support for autotools based build.
    git-svn-id: file:///home/adenilson/temp/wtf/repository@22 56e0a439-041b-0410-8790-949b9ad245cc

---
+++ thesis/clean.sh
@@ -0,0 +1,9 @@
+rm -fr aclocal.m4* config.* configure install-sh Makefile Makefile.in missing mkinstalldirs autom4te.cache/ COPYING INSTALL
+rm -fr *~
+rm -fr data/*txt data/*.png
+rm -fr build
+echo "Oh my" > NEWS
+echo "blessed" > README
+echo "and holy" > AUTHORS
+echo "God!" > ChangeLog
+
--- thesis/maker.sh
+++
@@ -1,5 +0,0 @@
-g++ -c -g contour.o contour.cpp
-g++ -c -g output.o output.cpp
-g++ -c -g descriptors.o descriptors.cpp
-g++ -c -g beta.o beta.cpp
-make gextractor
