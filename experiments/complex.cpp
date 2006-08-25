46521b11b3287a4ea7ce50c35b66398dab8dd161
adenilson <adenilson@56e0a439-041b-0410-8790-949b9ad245cc>
2006-08-25 01:45:11 +0000

    Adapted C++ std::complex class adding a overloaded operator for easy field setup of values.
    I think that we can close the complex numbers investigation.
    
    
    git-svn-id: file:///home/adenilson/temp/wtf/repository@31 56e0a439-041b-0410-8790-949b9ad245cc

--- experiments/complex.cpp
+++ experiments/complex.cpp
@@ -6,23 +6,81 @@
 void check4math(void);
 void test4ccomplex(void);
 
+/* There must exist a way to directly access private fields...
+ *  complex<double>::_M_value = _real;
+ *  complex<double>::_M_value = _img;
+ *
+ * At least complex<T>::real() is inline.
+ *
+ */
+template <class NUMBER>
+class mcomplex: public complex<NUMBER> {
+public:
+
+	/** Extra operator, make it easier to set values
+	 * into a complex number.
+	 *
+	 * @param real A real/integer number
+	 * @param img  A real/integer number for imaginary part
+	 *             of complex number
+	 *
+	 */
+	template <class TYPE>
+	void operator()(TYPE _real, TYPE _img)
+	{
+		complex<NUMBER>::real() = _real;
+		complex<NUMBER>::imag() = _img;
+	}
+
+	/** Overloaded operator, to do implicit convertion
+	 *
+	 *
+	 * @param c A complex number, should have methods
+	 *          c.real() and c.imag()
+	 *
+	 * @return A reference for this object
+	 */
+	template <class TYPE>
+	mcomplex<NUMBER> &operator=(std::complex<TYPE> &c) {
+		complex<NUMBER>::real() = c.real();
+		complex<NUMBER>::imag() = c.imag();
+		return *this;
+	}
+
+};
+
+
 int main(int argc, char* argv[])
 {
 	std::complex<double> c1(4.0, 3.0);
 	std::complex<float> c2(std::polar(5.0, 0.75));
 	std::complex<float> c3(0, 1);
+	std::complex<double> *ptr;
 
 
+	//Field access
+	mcomplex<double> a;
+	a = c3;
+	std::cout << "a: " << a << std::endl;
+	a(10, 300);
+	std::cout << "a: " << a << std::endl;
+	a = c1;
+	std::cout << "a: " << a << std::endl;
+	ptr = (std::complex<double> *) &a;
+	if (c1 == *ptr)
+		std::cout << "Nice\n";
+	if (c1 == a)
+		std::cout << "Perfect!\n";
 
-	//Field access??
-/*
-	c3.real(0);
-	c3.complex(1);
-*/
+	a = c3;
+	if ((a * a).real() == -1)
+		std::cout << "We are done!\n";
+
 	//Utility functions
 	std::cout << "c1: " << c1 << std::endl;
 	std::cout << "c2: " << c2 << std::endl;
 	std::cout << "c3: " << c3 << std::endl;
+
 
 	//ps: magnitude = sqrt(a^2 + b^2)
 	//    norma = a^2 + b^2
