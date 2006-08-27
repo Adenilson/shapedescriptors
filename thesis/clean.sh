e6d5e502b9f2d198dce6663af521a576d6ee2324
adenilson <adenilson@56e0a439-041b-0410-8790-949b9ad245cc>
2006-08-27 15:40:22 +0000

    Changed cleaner script, we don't need anymore to create extra files (e.g. NEWS, README, AUTHORS, Changelog, etc).
    git-svn-id: file:///home/adenilson/temp/wtf/repository@37 56e0a439-041b-0410-8790-949b9ad245cc

--- thesis/clean.sh
+++ thesis/clean.sh
@@ -1,11 +1,12 @@
-rm -fr aclocal.m4* config.* configure install-sh Makefile Makefile.in missing mkinstalldirs autom4te.cache/ COPYING INSTALL
+rm -fr aclocal.m4* config.* configure install-sh Makefile Makefile.in missing mkinstalldirs autom4te.cache/ COPYING INSTALL depcomp
 rm -fr *~
 rm -fr data/*txt data/*.png
 rm -fr build
 
-echo "Oh my" > NEWS
-echo "blessed" > README
-echo "and holy" > AUTHORS
-echo "God!" > ChangeLog
+#Automake-1.9 doesn't require this
+#echo "Oh my" > NEWS
+#echo "blessed" > README
+#echo "and holy" > AUTHORS
+#echo "God!" > ChangeLog
 
 mkdir build
