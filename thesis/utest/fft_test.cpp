be8caf054970a14130578107e6b4bfc3c9820431
adenilson <adenilson@56e0a439-041b-0410-8790-949b9ad245cc>
2006-09-04 00:18:37 +0000

    Added unit test for invert transform.
    git-svn-id: file:///home/adenilson/temp/wtf/repository@47 56e0a439-041b-0410-8790-949b9ad245cc

--- thesis/utest/fft_test.cpp
+++ thesis/utest/fft_test.cpp
@@ -14,6 +14,34 @@ using namespace std;
 
 #include <check.h>
 
+START_TEST (invert)
+{
+	int res = 0;
+
+	//Original data
+	int size = 4;
+	float v[] = { 1, 1, 1, 1, 1 };
+	float V[] = { 5, 0, 0, 0, 0 };
+
+	//Complex number to hold results
+	mcomplex<double> *t_obj, *T_obj;
+	t_obj = new mcomplex<double> [size];
+	T_obj = new mcomplex<double> [size];
+	for (int i = 0; i < size; ++i)
+		T_obj[i](V[i], 0);
+
+	//Transform and check for expected results
+	invert(T_obj, size, t_obj);
+	for (int i = 0; i < size; ++i)
+		if (v[i] != t_obj[i].real())
+		    res = 1;
+
+	fail_unless(res == 0, "failed invert transform");
+
+}
+END_TEST
+
+
 START_TEST (transf)
 {
 	int res = 0;
@@ -50,6 +78,7 @@ Suite *test_suite(void)
 
 	suite_add_tcase(s, test_case);
 	tcase_add_test(test_case, transf);
+	tcase_add_test(test_case, invert);
 	return s;
 }
 
