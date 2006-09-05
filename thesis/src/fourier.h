25159f49701a99791d2095a1a03acfb026204900
adenilson <adenilson@56e0a439-041b-0410-8790-949b9ad245cc>
2006-09-05 07:37:07 +0000

    Added a thread safe version of fourier transform function as also a utest for it. What is next: -- do a inverse thread safe function -- doxygen comments -- a utest with 2 transforms
    git-svn-id: file:///home/adenilson/temp/wtf/repository@50 56e0a439-041b-0410-8790-949b9ad245cc

--- thesis/src/fourier.h
+++ thesis/src/fourier.h
@@ -14,6 +14,7 @@
 #define _FOURIER_H
 
 #include <fftw3.h>
+#include <pthread.h>
 
 /** It does fourier transform in a given vector. Pay attention that its not
  *  thread safe since fftw_plan_dft functions does share some global data.
@@ -28,7 +29,6 @@
  * @param G Transformed signal, we expect a pre-allocated
  *          vector compatible with type 'double o[2]'.
  *
- * TODO: do a mutex lock when calling fftw_plan and make it thread safe.
  */
 template <class TYPE1, class TYPE2>
 void transform(TYPE1 g, int length, TYPE2 G)
@@ -43,6 +43,37 @@ void transform(TYPE1 g, int length, TYPE2 G)
 	fwd_plan = fftw_plan_dft_1d(length, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
 	fftw_execute(fwd_plan);
 
+	fftw_destroy_plan(fwd_plan);
+}
+/** Thread safe version of transform.
+ *
+ * @param g A vector with signal to be transformed, we
+ *          wait for a object/vector where sinal is accessable
+ *          with g[0][0] for real part and g[0][1] for
+ *          imaginary part.
+ *
+ * @param length Signal length.
+ *
+ * @param G Transformed signal, we expect a pre-allocated
+ *          vector compatible with type 'double o[2]'.
+ *
+ * @param mutex A mutex pointer variable for locking fftw_plan creation.
+ */
+template <class TYPE1, class TYPE2, class TYPE3>
+void transform(TYPE1 g, int length, TYPE2 G, TYPE3 *mutex)
+{
+
+	fftw_plan fwd_plan;
+	fftw_complex *in, *out;
+
+	in = reinterpret_cast<fftw_complex *>(g);
+	out = reinterpret_cast<fftw_complex *>(G);
+
+	pthread_mutex_lock(mutex);
+	fwd_plan = fftw_plan_dft_1d(length, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
+	pthread_mutex_unlock(mutex);
+
+	fftw_execute(fwd_plan);
 	fftw_destroy_plan(fwd_plan);
 }
 
--- thesis/utest/fft_test.cpp
+++ thesis/utest/fft_test.cpp
@@ -14,6 +14,65 @@ using namespace std;
 
 #include <check.h>
 
+struct function_param
+{
+	mcomplex<double> *signal;
+	mcomplex<double> *transf;
+	int size;
+	pthread_mutex_t *mutex;
+};
+
+void *thread_transform(void *param)
+{
+	function_param *obj = (function_param *) param;
+	transform(obj->signal, obj->size, obj->transf, obj->mutex);
+	return NULL;
+
+}
+
+START_TEST (thread_transf)
+{
+	int res = 0, tid;
+	pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
+	pthread_t thread1;
+	function_param parameters;
+
+	//Original data
+	int size = 4;
+	float v[] = { 1, 1, 1, 1 };
+	float V[] = { 4, 0, 0, 0 };
+
+	//Complex number to hold results
+	mcomplex<double> *t_obj, *T_obj;
+	t_obj = new mcomplex<double> [size];
+	T_obj = new mcomplex<double> [size];
+	for (int i = 0; i < size; ++i)
+		t_obj[i](v[i], 0);
+
+	//Create thread and pass parameters
+	parameters.signal = t_obj;
+	parameters.transf = T_obj;
+	parameters.mutex = &mutex1;
+	parameters.size = size;
+	tid = pthread_create(&thread1, NULL, thread_transform, &parameters);
+	if (tid) {
+		fail_unless(true, "transform(thread): failed thread creation!");
+
+	} else {
+		//Wait for thread and check for expected results
+		pthread_join(thread1, NULL);
+		for (int i = 0; i < size; ++i)
+			if (V[i] != T_obj[i].real())
+				res = 1;
+
+		fail_unless(res == 0, "failed transform");
+	}
+
+}
+END_TEST
+
+
+
 START_TEST (invert)
 {
 	int res = 0;
@@ -79,6 +138,7 @@ Suite *test_suite(void)
 	suite_add_tcase(s, test_case);
 	tcase_add_test(test_case, transf);
 	tcase_add_test(test_case, invert);
+	tcase_add_test(test_case, thread_transf);
 	return s;
 }
 
