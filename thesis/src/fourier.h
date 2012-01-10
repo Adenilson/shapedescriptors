0d93779fdc058873ba3e31f3469722bea97b8117
Adenilson Cavalcanti <cavalcantii@gmail.com>
2012-01-10 12:09:21 -0400

    Adding license to source code.
    Signed-off-by: Adenilson Cavalcanti <cavalcantii@gmail.com>

--- thesis/src/fourier.h
+++ thesis/src/fourier.h
@@ -16,6 +16,23 @@
  * possible.
  */
 
+/*  Copyright (C) 2006  Adenilson Cavalcanti <cavalcantii@gmail.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; by version 2 of the License.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
 #ifndef _FOURIER_H
 #define _FOURIER_H
 
